c17f56bce5a03fa2147d5a74d8d73f77081e00b3
Andrei Dubovik <andrei@dubovik.eu>
2022-01-10 19:01:56 +0100

    Computed ship's worldine

--- engine.js
+++ engine.js
@@ -1,4 +1,69 @@
-"use strict"
+"use strict";
+
+// All array operations assume rectangular arrays
+
+/** Apply an operator to a vector */
+function dot_ij_j(a1, a2) {
+    if (a1[0].length != a2.length) {
+        throw new Error("Non-conformant arrays");
+    }
+    let a0 = [];
+    for (let i = 0; i < a1.length; i++) {
+        a0[i] = 0;
+        for (let j = 0; j < a1[0].length; j++) {
+            a0[i] += a1[i][j]*a2[j];
+        }
+    }
+    return a0;
+}
+
+
+/** Multiply a vector by a scalar */
+function dot_0_i(a1, a2) {
+    let a0 = [];
+    for (let i = 0; i < a2.length; i++) {
+        a0[i] = a1*a2[i];
+    }
+    return a0;
+}
+
+
+/** Add a vector to an exisitng vector in-place */
+function addAssign(a0, a1) {
+    if (a0.length != a1.length) {
+        throw new Error("Non-conformant arrays");
+    }
+    for (let i = 0; i < a0.length; i++) {
+        a0[i] += a1[i];
+    }
+}
+
+
+/** Construct a Lorentz operator */
+function lorentz(v) {
+    let vx = v[0];
+    let vy = v[1];
+    let v2 = vx**2 + vy**2;
+    if (v2 >= 1) {
+        throw new Error("Cannot use Lorentz transformation for v >= c");
+    }
+    if (v2 < 1e-16) {
+        // Use small v approximation that is continuous at v == 0
+        return [
+            [  1, -vx, -vy],
+            [-vx,   1,   0],
+            [-vy,   0,   1],
+        ];
+    } else {
+        let g = 1/Math.sqrt(1 - v2);
+        return [
+            [    g,            -g*vx,            -g*vy],
+            [-g*vx, 1+(g-1)*vx**2/v2,   (g-1)*vx*vy/v2],
+            [-g*vy,   (g-1)*vx*vy/v2, 1+(g-1)*vy**2/v2],
+        ];
+    }
+}
+
 
 /** Track and show FPS */
 function updateFPS(state) {
@@ -76,11 +141,25 @@ function scheduledAcc(properTime) {
 }
 
 
+/** Change ship's position over a small time interval */
+function stepShip(ship, dtau) {
+    addAssign(ship.pos, dot_0_i(dtau, ship.vel));  // x += dx/dtau*dtau
+    addAssign(ship.vel, dot_0_i(dtau, ship.acc));  // v += dv/dtau*dtau
+
+    // v = - dxy/dtau / dt/dtau = - dxy/dt
+    let v = dot_0_i(-1/ship.vel[0], ship.vel.slice(1, 3));
+    let Linv = lorentz(v);
+    ship.acc = dot_ij_j(Linv, ship.ownAcc);
+}
+
+
 /** Update the simulation and the interface */
 function updateFrame(timestamp, previousTimestamp, state) {
     updateFPS(state);
-    state.properTime += dProperTime(timestamp, previousTimestamp);
-    state.ownAcc = scheduledAcc(state.properTimer);
+    let dtau = dProperTime(timestamp, previousTimestamp);
+    state.properTime += dtau;
+    state.ship.ownAcc = scheduledAcc(state.properTime);
+    stepShip(state.ship, dtau);
     updateAllClocks(state);
     window.requestAnimationFrame(t => updateFrame(t, timestamp, state));
 }
